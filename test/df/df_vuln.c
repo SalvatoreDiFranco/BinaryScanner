74a15de1444a203f285314ec67d4472edae679f2
SalvatoreDiFranco <121877661+SalvatoreDiFranco@users.noreply.github.com>
2024-07-02 10:27:46 +0200

    Update df_vuln.c

--- test/df/df_vuln.c
+++ test/df/df_vuln.c
@@ -2,11 +2,9 @@
 #include <stdlib.h>
 
 int main() {
-    // Allocazione di memoria per un intero
+    
     int *ptr = (int *)malloc(sizeof(int));
-    // Liberiamo la memoria allocata
     free(ptr);
-    // Tentativo di liberare nuovamente la stessa memoria (vulnerabilità double free)
     free(ptr);
 
     return 0;
